85f322b361ff47f82f9ae3168e3605696734c555
kateni-abe <kateni.info@gmail.com>
2023-09-09 10:02:13 +0900

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DeeplApi from "./DeeplApi";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Learning</h1>
+      <DeeplApi />
+      {/* DeeplApiコンポーネントを表示 */}
+    </div>
+  );
+};
+
+export default App;
